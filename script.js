7e4b577b5f796a4041de7c51ab8a31d52e69ea93
Pratiksh Saini <pratikshsaini10@gmail.com>
2025-07-26 07:17:46 +0530

    Minor Changes in Home Page

--- script.js
+++ script.js
@@ -64,6 +64,8 @@ const product = [
   },
 ];
 
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
 const productLoader = document.getElementById("product-loader");
 
 function loadProduct(productlist) {
@@ -77,7 +79,7 @@ function loadProduct(productlist) {
           <a href="#">${product.title}</a>
           <span>${product.price}</span>
           <div class="buttons flex justify-between">
-            <button class="w-28 h-11 bg-blue-500 text-white rounded">
+            <button id="addCart" class="w-28 h-11 bg-blue-500 text-white rounded" data-id="${product.id}">
               Add to Cart
             </button>
             <button class="w-28 h-11 bg-yellow-500 text-black rounded">
@@ -92,3 +94,45 @@ function loadProduct(productlist) {
 }
 
 loadProduct(product);
+
+document.querySelectorAll(".addCart").forEach((btn) => {
+  btn.addEventListener("click", function handleAddCart(e) {
+    const productId = parseInt(e.target.dataset.id);
+    const selectedProduct = product.find((p) => p.id === productId);
+
+    cart.push(selectedProduct);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(`${selectedProduct.title} added to cart!`);
+  });
+});
+
+const cartitems = JSON.parse(localStorage.getItem("cart")) || [];
+
+function displayCartItem() {
+  const cartContainer = document.getElementById("cart-container");
+  cartContainer.innerHTML = "";
+
+  if (cartitems.length === 0) {
+    cartContainer.innerHTML = "<p>Your cart is empty</p>";
+    return;
+  }
+
+  cartitems.forEach((items) => {
+    cartContainer.innerHTML += `
+  <div class="cart-product1 w-full h-32 flex justify-between items-center gap-2.5 border border-gray-300 pt-7 pb-7 pr-5 pl-5">
+    <div class="product-name1 w-2/5">${item.name}</div>
+    <div class="choose-quantity1 flex flex-col w-1/5">
+    <select class="border border-gray-300 rounded h-10" name="" id="">
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+              </select>
+            </div>
+            <div class="subtotal1 w-1/5">${item.price}</div>
+            <div class="remove1 w-1/5">Remove</div>
+          </div>
+  `;
+  });
+}
+
+displayCartItem();
